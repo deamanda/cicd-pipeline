f9b86e8ddc833ad65b71ae0c7f031bff3ff2d4c3
deamanda <deamanda@ya.ru>
2026-09-12 01:45:02 +0400

    sharedlib

--- vars/deploy_to_branch.groovy
+++ vars/deploy_to_branch.groovy
@@ -1,4 +1,4 @@
-def deploy_to_branch(name, image, port){
+def call(name, image, port){
     sh "docker pull ${image}"
     sh "docker rm -f ${name} || true"
     sh "docker run -d --name ${name} --expose ${port} -p ${port}:3000 ${image}"
